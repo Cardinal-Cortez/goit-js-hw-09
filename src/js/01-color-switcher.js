1419dc8ac38c54a8ed43b51ecfae58b3d5a26de1
Illia Bidiuk <Core778@gmail.com>
2023-03-19 17:47:52 +0200

    first commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+let startBtn = document.querySelector("button[data-start]");
+let stopBtn = document.querySelector("button[data-stop]");
+const btnChangeColor = document.querySelector('.body');
 
+let timerId = null;
+stopBtn.disabled = true;
+
+startBtn.addEventListener("click", () => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    timerId = setInterval(() =>{
+    document.body.style.backgroundColor = getRandomHexColor()}, 1000);   
+});
+
+stopBtn.addEventListener("click", () => {
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+    clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
