089d6a499fc4e90613ff4a215f56ec9605cfd023
Cardinal-Cortez <115037067+Cardinal-Cortez@users.noreply.github.com>
2023-03-26 16:36:40 +0000

    Deploying to gh-pages from @ Cardinal-Cortez/goit-js-hw-09@35549d16ed12f294017b5085b9f1aa8fb43549c1 🚀

---
+++ 03-promises.40071e4c.js
@@ -0,0 +1,2 @@
+!function(){var n=document.querySelector(".form");n.addEventListener("input",(function(){})),n.addEventListener("submit",(function(n,t){Math.random()}))}();
+//# sourceMappingURL=03-promises.40071e4c.js.map
