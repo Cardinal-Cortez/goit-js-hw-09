bec2e736f9253c0ccf13af3cc86568d761956982
Illia Bidiuk <Core778@gmail.com>
2023-04-02 00:26:04 +0300

    first commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,11 +1,35 @@
-const form = document.querySelector(".form");
-form.addEventListener("input",() => {});
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, delay);
+  });
+  promise
+    .then(() => console.log(`✅ Fulfilled promise ${position} in ${delay}ms`))
+    .catch(() => console.log(`❌ Rejected promise ${position} in ${delay}ms`));
+  return promise;
+}
+
+function startPromise(event) {
+  event.preventDefault();
+  const firstDelay = Number(form.querySelector('input[name = "delay"]').value);
+  const delayStep = Number(form.querySelector('input[name = "step"]').value);
+  const amount = Number(form.querySelector('input[name = "amount"]').value);
+
+  let delay = firstDelay;
+  const promises = [];
+  for (let position = 1; position <= amount; position++) {
+    const promise = createPromise(position, delay);
+    promises.push(promise);
+    delay += delayStep;
   }
 }
-form.addEventListener("submit", createPromise);
+
+form.addEventListener("submit", startPromise);
